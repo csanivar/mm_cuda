762aa3349b7802c8d58671de2d3174eb36b91ccc
themouli <mouli.4543@gmail.com>
2016-04-14 16:32:29 +0530

    Change  grid size

--- mm_cuda.cu
+++ mm_cuda.cu
@@ -89,7 +89,7 @@ void multiplyMatrixHost(const ul a[N][N], const ul b[N][N], ul c[N][N]) {
     cudaMemcpy(dC, c, N * N * sizeof(ul), cudaMemcpyHostToDevice);
 
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 dimGrid(N + dimBlock.x - 1 / dimBlock.x, N + dimBlock.y - 1 / dimBlock.y);
+    dim3 dimGrid(N/dimBlock.x, N/dimBlock.y);
 
     multiplyMatrixDevice<<<dimGrid, dimBlock>>>(dA, dB, dC);
     cudaThreadSynchronize();
